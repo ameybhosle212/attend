9efca1d4780acf11663966b5717295cdd957abe9
ameybhosle212 <ameybhosle212@gmail.com>
2021-07-14 21:58:55 +0530

    THIRD Commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(session({
 app.use("/",require('./routes/route'))
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log("SEVER at 2000")
